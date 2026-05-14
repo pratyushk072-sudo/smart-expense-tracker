5d76836b866436280800ea90287e9e7777045767
Pratyush <pratyushk072@gmail.com>
2026-05-14 18:09:09 +0530

    Done Delete the expense of the uses

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -47,4 +47,39 @@ router.get("/", authMiddleware, async (req, res) => {
     }
 });
 
+// DELETE EXPENSE
+router.delete("/:id", authMiddleware, async (req, res) => {
+    try {
+
+        // find expense
+        const expense = await Expense.findById(req.params.id);
+
+        // check expense exists
+        if (!expense) {
+            return res.status(404).json({
+                message: "Expense not found",
+            });
+        }
+
+        // check ownership
+        if (expense.user.toString() !== req.user.id) {
+            return res.status(401).json({
+                message: "Not authorized",
+            });
+        }
+
+        // delete expense
+        await expense.deleteOne();
+
+        res.status(200).json({
+            message: "Expense deleted successfully",
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+});
+
 module.exports = router;
