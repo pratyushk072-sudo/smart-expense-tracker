14c1ec1a8708330bdaeecad263b3a7e6b8f4fdff
Pratyush <pratyushk072@gmail.com>
2026-05-28 13:01:44 +0530

    Fixed analytics package

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 import { Toaster } from "react-hot-toast";
 import { Analytics } from "@vercel/analytics/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-
     <BrowserRouter>
       <App />
       <Analytics />
       <Toaster position="top-right" />
     </BrowserRouter>
-    
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
