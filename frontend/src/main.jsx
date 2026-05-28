e40771ee1a4aa6f6dcd04a9c4867323db3d090bf
Pratyush <pratyushk072@gmail.com>
2026-05-28 13:08:20 +0530

    Added speed insights

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,14 @@ import App from "./App";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
       <Analytics />
+      <SpeedInsights />
       <Toaster position="top-right" />
     </BrowserRouter>
   </React.StrictMode>
