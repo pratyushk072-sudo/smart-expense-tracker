6eca55be39aa491a16477b9367453f69912f4176
Pratyush <pratyushk072@gmail.com>
2026-05-28 12:00:44 +0530

    Connected frontend with deployed backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://smart-expense-tracker-8ytq.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
