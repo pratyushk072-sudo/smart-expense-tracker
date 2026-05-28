089fd7f894b284b7ae19c4a32b97585650ea2908
Pratyush <pratyushk072@gmail.com>
2026-05-28 12:31:35 +0530

    Added Vercel analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
     <BrowserRouter>
       <App />
+      <Analytics />
       <Toaster position="top-right" />
     </BrowserRouter>
     
